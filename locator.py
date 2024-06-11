6095604fbd38aa94435698cc3501a77f9b0fb270
Apsara <103523906+apgstocks@users.noreply.github.com>
2024-06-11 21:46:16 +0530

    Update locator.py

--- locator.py
+++ locator.py
@@ -1,4 +1,5 @@
-image=r'/Users/apsara/Documents/ME_image.jpeg'
+#image=r'/Users/apsara/Documents/ME_image.jpeg'
 #excel=r'/Users/apsara/Documents/meat_eat_customers.xlsx'
+image=r"https://github.com/apgstocks/ME_ad_campaign/blob/main/ME_image.jpeg"
 excel=r'/Users/apsara/Documents/test.xlsx'
 message=r'/Users/apsara/Documents/ME_message.docx'
